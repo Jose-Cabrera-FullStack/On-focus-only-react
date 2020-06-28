20d92f74a7464a3f1c2cda5f1501a5f5a4e59d40
Jose Cabrera <murdoc.jose.6@gmail.com>
2020-06-28 14:50:41 -0300

    se implemento responsive design en todas las paginas maquetadas

--- src/components/Youtube/Hero.jsx
+++ src/components/Youtube/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => (
   <section className="podcast__hero podcast__hero--youtube">
   <div className="podcast__hero__inside">
   <h1 className="podcast__hero__title">Lorem ipsum dolor sit amet sed diam nonummy.</h1>
-  <div className="flex">
+  <div className="flex display__screen__mobile__without__flex">
       <p className="podcast__hero__text">Lorem ipsum dolor sit amet, consectse diam nonummy nibh euismod tincidunt.</p>
       <div className="flex">
           <p className="podcast__hero__text__song podcast__hero__text__song--youtube">Miralo en</p>
--- src/components/Youtube/Information.jsx
+++ src/components/Youtube/Information.jsx
@@ -9,7 +9,7 @@ const Information = () => (
         <section className="podcast__information">
                 <h1>Lorem ipsum dolor sit amet sed diam, amet sed diam nonummy.</h1>
                 <p className="podcast__information__text">Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo trud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolo augue duis dolore te feugait nulla facilisi.</p>
-                <img src={Img} alt=""/>
+                <img className="podcast__information__img" src={Img} alt="Imagen del curso"/>
                 <p className="podcast__information__text">Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolo augue duis dolore te feugait nulla facilisi.</p>
                 <br/>
                 <OtherCourse/>
