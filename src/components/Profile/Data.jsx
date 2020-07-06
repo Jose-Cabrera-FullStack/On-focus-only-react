bba3537ab340177b947c25e4af114d13a85d0a74
Jose Cabrera <murdoc.jose.6@gmail.com>
2020-07-06 17:41:34 -0300

    se debe arreglar el formulario de myProfile

--- src/components/Profile/Data.jsx
+++ src/components/Profile/Data.jsx
@@ -1,26 +1,97 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import '../../assets/styles/components/Data.scss';
 import Button from '../../components/Utils/ButtonArrowRight'
 import Check from '../../assets/static/images/svg/icon-check.svg'
-import Message from  '../../assets/static/images/svg/send-message.svg';
+
+import Success from '../Utils/SuccessMessage'
 
 
+const Data = (props) => {
 
-const Data = () => {
+    const [isForm, SetIsForm]= useState({
+        actualPassword: '',
+        newPassword: '',
+        newPasswordConfirmation: '',
+    })
+
+    const [isFormValidate, SetIsFormValidate]= useState({
+        actualPasswordValidate: false,
+        newPasswordValidate: false,
+        newPasswordConfirmationValidate: false,
+    })
+
+    const handleChange = e => {
+        
+        SetIsForm({
+            ...isForm,
+            [event.target.name]: event.target.value
+        })
+        
+        conditionalFormActual()
+        
+    }
+
+    const conditionalFormActual = ()=> {
+
+        const actualPassword = isForm.actualPassword
+
+        if(actualPassword.length > 6 && /[A-Z]/.test(actualPassword)){
+            SetIsFormValidate({actualPasswordValidate:true})
+        }else{
+            SetIsFormValidate({actualPasswordValidate:false})
+        }
+
+    }
+    
+    const handleSubmit = event => {
+        event.preventDefault()
+        // props.loginUser(form, '/perfil')
+    }
+
+
+    console.log(isFormValidate.actualPasswordValidate)
+
   return (
-    <section className="data">
+    <section className="data" onSubmit={handleSubmit}>
         <h4>Gestioná tus datos.</h4>
         <h5>Cambiar contraseña.</h5>
         <form action="">
-            <input className="data__input__text" type="text" placeholder="Contraseña actual"/><img className="data__input__check" src={Check} alt=""/> <br/>
-            <input className="data__input__text" type="text" placeholder="Nueva Contraseña"/><img className="data__input__check" src={Check} alt=""/><br/>
-            <input className="data__input__text" type="text" placeholder="Confirmar Nueva Contraseña"/><img className="data__input__check" src={Check} alt=""/><br/>
-            <Button text={'Guardar'} margin={"discovery__box__position btn__secundary--data"}/>
-            <div className="contact__check__message flex">
-                    <img src={Message} alt=""/>
-                    <p>Tu contraseña nueva ha sido guardada con éxito. </p>
-                </div>
+            <input 
+            name="actualPassword" 
+            required 
+            onChange={handleChange} 
+            className="data__input__text" 
+            type="password" 
+            placeholder="Contraseña actual"/>
+            {isFormValidate.actualPasswordValidate ? <img className="data__input__check" src={Check} alt=""/>:"" }
+            
+            
+            <br/>
+
+            <input name="newPassword" 
+            required 
+            onChange={handleChange} 
+            className="data__input__text" 
+            type="password" 
+            placeholder="Nueva Contraseña"/>
+            <img className="data__input__check" src={Check} alt=""/>
+
+            <br/>
+
+            <input name="newPasswordConfirmation"
+            required onChange={handleChange} 
+            className="data__input__text" 
+            type="password" 
+            placeholder="Confirmar Nueva Contraseña"/>
+            <img className="data__input__check" src={Check} alt=""/>
+            
+            <br/>
+
+            <Button text={"Guardar"} margin={"discovery__box__position btn__secundary--data"}/>
+
+           <Success/>
+
         </form>
     </section>
 )}
---
+++ src/components/Utils/SuccessMessage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../../assets/styles/components/Utils.scss';
+
+import Message from  '../../assets/static/images/svg/send-message.svg';
+
+
+const SuccessMessage = (props) => {
+  return (
+        <div className="contact__check__message flex">
+            <img src={Message} alt=""/>
+            <p>Tu contraseña nueva ha sido guardada con éxito. </p>
+        </div>
+)}
+export default SuccessMessage;
