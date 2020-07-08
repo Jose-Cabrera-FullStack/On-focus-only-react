430c3fd050358fb9da5a0f6144b3472530176731
Jose Cabrera <murdoc.jose.6@gmail.com>
2020-07-08 13:03:14 -0300

    se va a arreglar ciertos aspectos de la UI antes de comenzar la implementacion de Redux

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -8,54 +8,60 @@ import Button from '../components/Utils/ButtonArrowRight'
 
 import Logo from '../assets/static/images/img/logo-log-in.png'
 import Human from '../assets/static/images/svg/human-hello.svg'
-import Arrow from '../assets/static/images/svg/arrow-right.svg'
 import '../assets/styles/components/Register.scss';
 
 const EMAIL_REGEX = new RegExp(
   /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-)
-
+  )
+const Register = (props) => {
+  
 const handleInput = event => {
-  handleValidation(event.target)
 
-  setValues({
-    ...form,
+  setForm({
+    ...isForm,
     [event.target.name]: event.target.value
   })
 }
 
 const handleSubmit = event => {
   event.preventDefault()
-  props.registerUser(form, '/login')
+  props.registerUser(isForm, '/login')
 }
+const [isForm, setForm] = useState({
+  email: '',
+  password: '',
+})
 
-const Register = (props) => {
-  const [form, setValues] = useState({
-    email: '',
-    name: '',
-    password: '',
-    birthday: '',
-    server: 'LAS',
-    verified: false,
-    created_at: moment().format()
-  })
+console.log(isForm.email)
+console.log(isForm.password)
 
   return (
       <section className="register">
-          <img className="register__img" src={Logo} alt=""/>
+          <img className="register__img" src={Logo} alt="Logo de On Focus"/>
           <div className="register__box">
             <div className="register__box__inside">
-              <img className="register__box__inside__img" src={Human} alt=""/>
+              <img className="register__box__inside__img" src={Human} alt="Imagen de Humano Saludando"/>
               <h1>Hola!</h1>
               <h2>Lorem ipsum dolor sit amet.</h2>
-              <form action="" >
+              <form onSubmit={handleSubmit} >
                 <div>
                   <h5>O registrate con tu correo electrónico</h5>
-                  <input type="text" placeholder="tucorreo@gmail.com"/>
+                  <input 
+                  required
+                  name="email"
+                  type="text" 
+                  placeholder="tucorreo@gmail.com" 
+                  onChange={handleInput}/>
+
                 </div>
                 <div>
                   <h5>Contraseña</h5>
-                  <input type="text" placeholder="Contraseña"/>
+                  <input 
+                  required
+                  name="password"
+                  type="password" 
+                  placeholder="Contraseña"
+                  onChange={handleInput}/>
                 </div>
                 <Button margin={'btn__secundary--register'} text={'Registrarme'}/>
               </form>
