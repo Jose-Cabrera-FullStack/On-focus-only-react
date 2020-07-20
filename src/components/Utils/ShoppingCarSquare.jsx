4622ad763d2c5ef09f61e1f3128fa1a992179c7d
Jose Cabrera <murdoc.jose.6@gmail.com>
2020-07-20 15:39:13 -0300

    preparando para enviar a Jacsiel

--- src/components/Utils/ShoppingCarSquare.jsx
+++ src/components/Utils/ShoppingCarSquare.jsx
@@ -32,27 +32,34 @@ const ShoppingCarSquare = (props) => {
 
   const handleShopping = () => {
     setIsToggle(!isToggle);
+
     props.setFavorite({
-    category,
-    course_id,
-    creation_date,
-    description,
-    discount,
-    duration,
-    entity_status,
-    featured_image,
-    module,
-    name,
-    price,
-    status,
-    students,
-    teacher,
-    video_intro,
+      category,
+      course_id,
+      creation_date,
+      description,
+      discount,
+      duration,
+      entity_status,
+      featured_image,
+      module,
+      name,
+      price,
+      status,
+      students,
+      teacher,
+      video_intro,
     });
   };
+
+  const handleDeleteShopping = (course_id) => {
+    setIsToggle(!isToggle);
+    props.deteleFavorite(course_id);
+  };
+
   return (
     <div
-      onClick={handleShopping}
+      onClick={isToggle ? handleShopping :() => handleDeleteShopping(course_id)}
       className={" discovery__box__info__icon" + " " + props.class}
       alt="Shopping Car"
     >
