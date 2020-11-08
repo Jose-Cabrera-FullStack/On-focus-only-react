eeb0397687de49468e2e80af79e787fbe4074d77
Jose <jose.cabrera@bigbox.com.ar>
2020-11-08 20:07:47 -0300

    se arreglo el webpack para produccion

--- webpack.prod.js
+++ webpack.prod.js
@@ -43,7 +43,7 @@ module.exports = {
         },
       },
       {
-        test: /\.(jpg|png)$/,
+        test: /\.(jpg|png|svg)$/,
         exclude: /node_modules/,
         loader: "file-loader",
         options: {
@@ -53,25 +53,25 @@ module.exports = {
           outputPath: "img/",
         },
       },
-      {
-        test: /\.svg$/,
-        loader: "svg-url-loader",
-        options: {
-          // Images larger than 10 KB won’t be inlined
-          limit: 10 * 1024,
-          // Remove quotes around the encoded URL –
-          // they’re rarely useful
-          noquotes: true,
-        },
-      },
-      {
-        test: /\.(jpg|png|gif|svg)$/,
-        loader: "image-webpack-loader",
-        // Specify enforce: 'pre' to apply the loader
-        // before url-loader/svg-url-loader
-        // and not duplicate it in rules with them
-        enforce: "pre",
-      },
+      // {
+      //   test: /\.svg$/,
+      //   loader: "svg-url-loader",
+      //   options: {
+      //     // Images larger than 10 KB won’t be inlined
+      //     limit: 10 * 1024,
+      //     // Remove quotes around the encoded URL –
+      //     // they’re rarely useful
+      //     noquotes: true,
+      //   },
+      // },
+      // {
+      //   test: /\.(jpg|png|gif|svg)$/,
+      //   loader: "image-webpack-loader",
+      //   // Specify enforce: 'pre' to apply the loader
+      //   // before url-loader/svg-url-loader
+      //   // and not duplicate it in rules with them
+      //   enforce: "pre",
+      // },
     ],
   },
   devServer: {
